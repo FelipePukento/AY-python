494a7ae90e6a0248c7fc04e7bf136054d25623d1
Felipe Riveros <66898886+FelipePukento@users.noreply.github.com>
2025-04-17 10:20:34 -0400

    Add program to decode messages from numerical input

---
+++ src/examples/SOS-1704.py
@@ -0,0 +1,12 @@
+letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+'''
+A = 0   H = 7   O = 14  V = 21
+B = 1   I = 8   P = 15  W = 22
+C = 2   J = 9   Q = 16  X = 23
+D = 3   K = 10  R = 17  Y = 24
+E = 4   L = 11  S = 18  Z = 25
+F = 5   M = 12  T = 19
+G = 6   N = 13  U = 20
+'''
+'''Mediante for y while anidados, Realizar un probrama que decofique un mensaje mediante numeros separados por ,
+[ejemplo 18,14,18 seria S O S] el mensaje se debe mostrar en pantalla.'''
