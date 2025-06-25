cfdac36f7f13c78be4cae9d5e802d9f32b303d9b
felipepukento <friveros@usm.cl>
2025-06-24 21:07:54 -0400

    Add bird sighting records and implement species date tracking functionality

--- src/examples/Pre-Certamen/xd.py
+++ src/examples/Pre-Certamen/xd.py
@@ -1,28 +1,34 @@
-r_arch = open("src/examples/Pre-Certamen/pajaros.txt","r")
-v_pajaros = {
-    #"pajaro" : cant_veces
-}
-lista_p = []
-for line in r_arch:
-    dato = line.strip().split(":")
+'''5. [50 %] Escriba la función fechas-por-especie(nombre_archivo), que recibe como parámetro el nombre de 
+un archivo con el formato descrito. La función debe crear un archivo especies_por_fecha.txt,
+ que agrupe por fecha todas las especies observadas ese día,
+ ordenadas alfabéticamente. Las fechas deben aparecer ordenadas cronológicamente.'''
 
-    date = dato[0]
-    city = dato[1]
-    avistamientos = dato[2].split(",")
-    
-    for pajaro in avistamientos:
-        if pajaro not in v_pajaros:
-            v_pajaros[pajaro] = 1
-        else:
-            v_pajaros[pajaro] += 1
+#por cada especie, guardar en un archivo las fechas en las cual se registre
+#mes
 
 
+def fpe(nombre_archivo):
+    diccionario = {
+       #especie : [fechas]
+    }
+    archivo = open(nombre_archivo, "r", encoding="UTF-8")
+    for line in archivo:
+        data = line.strip().split(":")
+        date = data[0].split("/")
+        city = data[1]
+        especies = data[2].split(",")
+        for i in especies:
+            if i not in diccionario:
+                diccionario[i] = []
+            diccionario[i].append(date)
+    for key in diccionario:
+        nuevo_archivo = open(f"{key}_dias_visto.txt","w",encoding="UTF-8")
+        for date in diccionario[key]:
+            nuevo_archivo.write(f"El año {date[0]} en el mes {date[1]} en la fecha {date[2]} se vio un {key} \n")
 
-for i in v_pajaros:
-    lista_p.append([v_pajaros[i],i])
-lista_p.sort()
-lista_p.reverse()
-print(lista_p)
 
 
 
+
+
+fpe("src\examples\Pre-Certamen\pajaros.txt")
